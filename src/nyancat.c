387400bbbded59974fd0410d62fca2d08975524e
Kevin Lange <k@dakko.us>
2011-12-04 14:06:35 -0600

    Wow, that was dumb

--- src/nyancat.c
+++ src/nyancat.c
@@ -569,10 +569,10 @@ ready:
 		time(&current);
 		double diff = difftime(current, start);
 		/* Now count the length of the time difference so we can center */
-		int nLen = (int)diff;
+		int nLen = digits((int)diff);
 		int width = (80 - 29 - nLen) / 2;
 		/* Spit out some spaces so that we're actually centered */
-		while (width) {
+		while (width > 0) {
 			printf(" ");
 			width--;
 		}
